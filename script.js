dd5ee0a1191b52bbebee705ab1409b8ea3eb7a4d
vahid <vahidkosari28@gmail.com>
2021-02-08 11:27:27 +0330

    divide code to Painter and Comment functions

--- script.js
+++ script.js
@@ -46,146 +46,157 @@ const builder = {
   },
 }
 
-const comments = [
-  {
-    id: '1',
-    text:
-      'Great welcoming experience to the Dominican. We used Otium for the airport transport- we were to the resort in 20 minutes. Ralphy has been our concierge for the first half of the stay, he been very helpful- always able to pull through for anything we request',
-    username: 'Jake W',
-    imageurl:
-      'https://media-cdn.tripadvisor.com/media/photo-l/01/2e/70/9e/avatar069.jpg',
-    date: 'January 2020',
-    point: 11,
-  },
-  {
-    id: '2',
-    text:
-      'We travelled as a group of 13 ages 18 to 80 and this resort was enjoyed by all. Weve been fortunate to travel to many resorts in Punta Cana but the Grand was by far the best',
-    username: 'Andrea C',
-    imageurl:
-      'https://media-cdn.tripadvisor.com/media/photo-l/01/2e/70/55/avatar028.jpg',
-    date: 'Jan 2020',
-    point: 14,
-  },
-  {
-    id: '3',
-    text:
-      'The Hotel opened 1-1,5 hs ago. Everything is new and very clean. Even the cheapest rooms are very spacious (77square meters and room and dinning area with sofa bed are diivided by a sliding door).',
-    username: 'sergiogiaco',
-    imageurl:
-      'https://media-cdn.tripadvisor.com/media/photo-l/01/2e/70/74/avatar056.jpg',
-    date: 'Dec 2020',
-    point: 11,
-  },
-]
-
-function cardPro(username, imageurl) {
-  const pro = builder
-    .create('div')
-    .className('pro')
-  const image = builder
-    .create('div')
-    .className('image')
-    .appendTo(pro)
-  builder
-    .create('img')
-    .src(imageurl)
-    .appendTo(image)
-  builder
-    .create('p')
-    .className('name')
-    .text(username)
-    .appendTo(pro)
-  return pro
+function Comments() {
+  this.records = [
+    {
+      id: '1',
+      text:
+        'Great welcoming experience to the Dominican. We used Otium for the airport transport- we were to the resort in 20 minutes. Ralphy has been our concierge for the first half of the stay, he been very helpful- always able to pull through for anything we request',
+      username: 'Jake W',
+      imageurl:
+        'https://media-cdn.tripadvisor.com/media/photo-l/01/2e/70/9e/avatar069.jpg',
+      date: 'January 2020',
+      point: 11,
+    },
+    {
+      id: '2',
+      text:
+        'We travelled as a group of 13 ages 18 to 80 and this resort was enjoyed by all. Weve been fortunate to travel to many resorts in Punta Cana but the Grand was by far the best',
+      username: 'Andrea C',
+      imageurl:
+        'https://media-cdn.tripadvisor.com/media/photo-l/01/2e/70/55/avatar028.jpg',
+      date: 'Jan 2020',
+      point: 14,
+    },
+    {
+      id: '3',
+      text:
+        'The Hotel opened 1-1,5 hs ago. Everything is new and very clean. Even the cheapest rooms are very spacious (77square meters and room and dinning area with sofa bed are diivided by a sliding door).',
+      username: 'sergiogiaco',
+      imageurl:
+        'https://media-cdn.tripadvisor.com/media/photo-l/01/2e/70/74/avatar056.jpg',
+      date: 'Dec 2020',
+      point: 11,
+    },
+  ]
 }
 
-function error(type) {
-  alert(`you already ${type} it`)
-}
+function Painter(root) {
+  this.comments = new Comments();
 
-function click(operator, point, count) {
-  const n = +count.getText()
-  if (operator === 'like') {
-    if (n > point) {
-      error('like')
-      return
+  this.cardPro = function (username, imageurl) {
+    const pro = builder
+      .create('div')
+      .className('pro')
+    const image = builder
+      .create('div')
+      .className('image')
+      .appendTo(pro)
+    builder
+      .create('img')
+      .src(imageurl)
+      .appendTo(image)
+    builder
+      .create('p')
+      .className('name')
+      .text(username)
+      .appendTo(pro)
+    return pro
+  }
+
+  this.error = function (type) {
+    alert(`you already ${type} it`)
+  }
+
+  this.click = function (operator, point, count) {
+    const n = +count.getText()
+    if (operator === 'like') {
+      if (n > point) {
+        this.error('like')
+        return
+      }
+      count.text(n + 1)
+    } else if (operator === 'dislike') {
+      if (n < point) {
+        this.error('dislike')
+        return
+      }
+      count.text(n - 1)
     }
-    count.text(n + 1)
-  } else if (operator === 'dislike') {
-    if (n < point) {
-      error('dislike')
-      return
-    }
-    count.text(n - 1)
+  }
+
+  this.cardPoints = function (point) {
+    const points = builder
+      .create('div')
+      .className('points')
+    const p = builder
+      .create('p')
+      .text('Total Points: ')
+      .appendTo(points)
+    const count = builder
+      .create('span')
+      .className('count')
+      .text(point)
+      .appendTo(p)
+    builder
+      .create('span')
+      .className('like')
+      .text('👍')
+      .onClick(() => this.click('like', point, count))
+      .appendTo(p)
+    builder
+      .create('span')
+      .className('dislike')
+      .text('👎')
+      .onClick(() => this.click('dislike', point, count))
+      .appendTo(p)
+    return points
+  }
+
+  this.cardTop = function (username, imageurl, point) {
+    const top = builder
+      .create('div')
+      .className('top')
+    this.cardPro(username, imageurl).appendTo(top)
+    this.cardPoints(point).appendTo(top)
+    return top
+  }
+
+  this.cardBottom = function (text, date) {
+    const bottom = builder
+      .create('div')
+      .className('bottom')
+    builder
+      .create('p')
+      .className('text')
+      .text(text)
+      .appendTo(bottom)
+    builder
+      .create('p')
+      .className('date')
+      .text(date)
+      .appendTo(bottom)
+    return bottom
+  }
+  this.card = function ({ id, text, username, imageurl, date, point }) {
+    const comment = builder
+      .create('div')
+      .className('comment')
+      .id(id)
+    this.cardTop(username, imageurl, point).appendTo(comment)
+    this.cardBottom(text, date).appendTo(comment)
+    return comment
+  }
+
+  this.render = function () {
+    const commentList = this.comments.records.map((item) => this.card(item))
+    const container = builder
+      .create('div')
+      .className('container')
+      .appendTo(root)
+    commentList.forEach((item) => item.appendTo(container))
   }
 }
 
-function cardPoints(point) {
-  const points = builder
-    .create('div')
-    .className('points')
-  const p = builder
-    .create('p')
-    .text('Total Points: ')
-    .appendTo(points)
-  const count = builder
-    .create('span')
-    .className('count')
-    .text(point)
-    .appendTo(p)
-  builder
-    .create('span')
-    .className('like')
-    .text('👍')
-    .onClick(() => click('like', point, count))
-    .appendTo(p)
-  builder
-    .create('span')
-    .className('dislike')
-    .text('👎')
-    .onClick(() => click('dislike', point, count))
-    .appendTo(p)
-  return points
-}
-
-function cardTop(username, imageurl, point) {
-  const top = builder
-    .create('div')
-    .className('top')
-  cardPro(username, imageurl).appendTo(top)
-  cardPoints(point).appendTo(top)
-  return top
-}
-function cardBottom(text, date) {
-  const bottom = builder
-    .create('div')
-    .className('bottom')
-  builder
-    .create('p')
-    .className('text')
-    .text(text)
-    .appendTo(bottom)
-  builder
-    .create('p')
-    .className('date')
-    .text(date)
-    .appendTo(bottom)
-  return bottom
-}
-
-function card({ id, text, username, imageurl, date, point }) {
-  const comment = builder
-    .create('div')
-    .className('comment')
-    .id(id)
-  cardTop(username, imageurl, point).appendTo(comment)
-  cardBottom(text, date).appendTo(comment)
-  return comment
-}
-
-const commentList = comments.map((item) => card(item))
-const container = builder
-  .create('div')
-  .className('container')
-  .appendTo(document.getElementById('root'))
-commentList.forEach((item) => item.appendTo(container))
+const app = new Painter(document.getElementById('root'))
+app.render()
